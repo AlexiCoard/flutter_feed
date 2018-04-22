73e67e4d7db94cb028560f3a74ef32f009f17c54
Alexi Coard <alexicoard@gmail.com>
2018-04-22 11:30:11 +0200

    open tweet on click

--- lib/renderer/twitter_renderer.dart
+++ lib/renderer/twitter_renderer.dart
@@ -1,7 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class TwitterRenderer {
   TwitterRenderer();
+
+  _launchTwitterURL(String tweetId) async {
+    var fullURL = 'https://twitter.com/statuses/$tweetId';
+
+    if (await canLaunch(fullURL)) {
+      await launch(fullURL);
+    } else {
+      throw 'Could not launch $fullURL';
+    }
+  }
 
   Widget render(List data) {
     return new ListView(
@@ -12,6 +23,7 @@ class TwitterRenderer {
                 leading:
                     new Image.network(tweet["user"]["profile_image_url_https"]),
                 isThreeLine: true,
+                onTap: () => _launchTwitterURL(tweet["id_str"]),
               ))
           .toList(),
     );
